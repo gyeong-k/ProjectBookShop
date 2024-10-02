63c532d68357ddba773ec159e748bead28e17184
gayeongkim <gy.kim.g0@gmail.com>
2024-10-02 23:26:01 +0900

    로그인 구현 및 토큰 사용

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,8 @@
 const conn = require("../mariadb"); //현재폴더의 상위 폴더
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken"); //jwt 모듈
+const dotenv = require("dotenv"); //dotenv 모듈
+dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
@@ -16,4 +19,54 @@ const join = (req, res) => {
   });
 };
 
-module.exports = join;
+const login = (req, res) => {
+  const { email, password } = req.body;
+  let sql = "SELECT * FROM users WHERE email = ?";
+
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); //bad request
+    }
+    console.log("로그인 results : ", results);
+    const loginUser = results[0];
+
+    if (loginUser && loginUser.password == password) {
+      //토큰 발생
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "kk",
+        }
+      );
+      //토큰 쿠키에 담기
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+      console.log("token: ", token);
+
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).json(results); //401:Unauthorized비인증 / 403:Forbidden접근 관리 없음
+    }
+  });
+};
+
+const passwordResetRequest = (req, res) => {
+  res.status(200).send("비번 초기화 요청"); //
+};
+
+const passwordReset = (req, res) => {
+  res.status(200).send("비번 초기화"); //
+};
+
+module.exports = {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb"); //현재폴더의 상위 폴더
-
-const join = require("../controller/UserController");
+const {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset,
+} = require("../controller/UserController");
 
 router.use(express.json());
 
 //회원가입
 router.post("/join", join);
-
-router.post("/login", (req, res) => {
-  res.status(200).send("로그인"); //  토큰
-});
+//로그인
+router.post("/login", login);
 //비번 초기화 요청
-router.post("/reset", (req, res) => {
-  res.status(200).send("비번 초기화 요청"); //
-});
+router.post("/reset", passwordResetRequest);
 //비번 초기화 (수정)
-router.put("/reset", (req, res) => {
-  res.status(200).send("비번 초기화"); //
-});
+router.put("/reset", passwordReset);
 
 module.exports = router;
