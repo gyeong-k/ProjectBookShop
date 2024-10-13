46879d9da0d978f2cec78e3e743d4319b611e7c4
gayeongkim <gy.kim.g0@gmail.com>
2024-10-13 19:55:55 +0900

    좋아요 부분 구현

--- routes/likes.js
+++ routes/likes.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { addLike, removeLike } = require("../controller/LikeController");
 
 router.use(express.json());
 
-router.post("/:id", (req, res) => {
-  res.status(201).send("좋아요 추가");
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(201).send("좋아요 삭제");
-});
+router.post("/:id", addLike);
+router.delete("/:id", removeLike);
 
 module.exports = router;
